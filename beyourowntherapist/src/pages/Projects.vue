6be30cf4b0c237462a0e712f2a5c1a3b67303784
Navya Arora <n22arora@uwaterloo.ca>
2021-08-16 01:24:49 +0530

    Added sat prep project

---
+++ beyourowntherapist/src/pages/Projects.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content">
+    <div class="md-layout">
+      <!-- <div class="md-layout-item md-medium-size-100 md-size-66">
+        <edit-profile-form data-background-color="green"> </edit-profile-form>
+      </div> -->
+      <div class="md-layout-item md-medium-size-100 md-size-33">
+        <sat-prep> </sat-prep>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SatPrep from './Projects/SatPrep.vue';
+
+export default {
+  components: {
+    SatPrep
+  }
+};
+</script>
