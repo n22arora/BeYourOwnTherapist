9478efc6843667a02218b20f50fcd2340d09e9b9
Navya Arora <n22arora@uwaterloo.ca>
2021-08-16 21:55:11 +0530

    Changed name of table

--- portfolio/src/pages/AboutMe/OtherExperience.vue
+++ portfolio/src/pages/AboutMe/OtherExperience.vue
@@ -4,7 +4,7 @@
       <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
         <md-card>
           <md-card-header data-background-color="green">
-            <h4 class="title">Other Experience</h4>
+            <h4 class="title">Other Experiences</h4>
           </md-card-header>
           <md-card-content>
             <other-experience-table table-header-color="green"></other-experience-table>
